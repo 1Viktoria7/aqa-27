f21bad77b8ed97aa4b0c333143726f00e2358801
1Viktoria7 <vika-chibisova@mail.ru>
2023-07-20 17:00:36 +0300

    sync

--- src/test/java/ru/apteka/test/AptekaTest.java
+++ src/test/java/ru/apteka/test/AptekaTest.java
@@ -17,7 +17,6 @@ import static com.codeborne.selenide.Selenide.*;
 import static io.qameta.allure.Allure.step;
 
 public class AptekaTest extends WebTest {
-
     MainPage mainPage = new MainPage();
     CityPopup cityPopup = new CityPopup();
     CatalogPage catalogPage = new CatalogPage();
